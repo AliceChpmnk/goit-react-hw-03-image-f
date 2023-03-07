8f7f083385cdc0de037472e6252da9a0bf415f3b
AliceChpmnk <alisablog50@gmail.com>
2023-03-07 22:55:26 +0200

    state in gallery

--- src/services/pixabyApi.jsx
+++ src/services/pixabyApi.jsx
@@ -13,6 +13,6 @@ export const getPicturesByName = async (name, page) => {
         orientation: 'horizontal',
         safesearch: 'true',
     })
-    const {data} = await axios(`?${options}`);
-    return data;
+    const response = await axios(`?${options}`);
+    return response.data.hits;
 }
